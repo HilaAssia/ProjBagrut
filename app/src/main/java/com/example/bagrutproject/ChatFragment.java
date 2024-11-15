5fe325887409b67e6a3642e2996a5e83701ae932
HilaAssia <hila.assia@gmail.com>
2024-11-15 09:35:24 +0200

    Initial Commit

--- app/src/main/java/com/example/bagrutproject/ChatFragment.java
+++ app/src/main/java/com/example/bagrutproject/ChatFragment.java
@@ -59,6 +59,6 @@ public class ChatFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_product_listing, container, false);
+        return inflater.inflate(R.layout.fragment_chat, container, false);
     }
 }
